b3da1b53b9d547af2e3b2ebd8d408b29d152a939
Garret Gallant <gallanttechnological@gmail.com>
2022-08-09 23:26:51 -0600

    complete

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?('.')
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?('?')
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?('!')
+      return true
+    else
+      return false
+    end
   end
 
-  def count_sentences
+# attend study hall and inquire on this further; this is ALOT
 
+  def count_sentences
+    self.split(/\.|\?|\!/).filter { |sentence| !sentence.empty? }.size
   end
 end
